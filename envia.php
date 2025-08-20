21bd6d1e14628a7cb364b171391af4bea748dbea
Luiz Vilarinho <luizvilarinho948@gmail.com>
2025-08-20 11:22:07 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -10,13 +10,14 @@
 
     $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Assunto: ".$assunto."\n"."Mensagem: ".$mensagem;
 
-    $cabeca = "From: luizvilarinho948@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: luizfernandoc809@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
     if(mail($para,$assunto,$corpo,$cabeca)){
         echo("E-mail enviado com sucesso!");
     }else{
         echo("Houve um erro ao enviar o email!");
     }
+
 
 
 ?>
